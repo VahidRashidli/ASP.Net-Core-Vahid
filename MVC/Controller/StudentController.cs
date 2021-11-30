c2909fbe88ee56e2cde9f1599cd2d61aab4be28a
Vahid <vrashidli1@gmail.com>
2021-12-01 00:15:44 +0400

    Done

---
+++ MVC/Controller/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index(Nullable<int> id)
+        {
+            if (id==null)
+            {
+                ViewData["Info"] = "Id is null";
+                return View();
+            }
+
+            return Json(new {Name="Vahid" ,Surname="Rashidli", Age=20});
+        }
+    }
+}
